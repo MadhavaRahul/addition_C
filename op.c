1b990d4af90d67ef9e93e5137a8318b72a58772b
MadhavaRahul <149048809+MadhavaRahul@users.noreply.github.com>
2024-06-18 22:42:28 +0530

    Add files via upload

---
+++ op.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter a value:")
+    scanf("%d",&a);
+    printf("enter b value:")
+    scanf("%d",&b);
+    c=a+b;
+    printf("c=%d",c);
+}
